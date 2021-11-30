b3cb9f8c5823192a9ccb695f981f15559e03b983
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2021-11-30 22:32:54 +0900

    Fix the permission error by updating how OIDC token are retrived

--- main.py
+++ main.py
@@ -1,44 +1,25 @@
-from functools import wraps
-import logging
 import os
-import time
-
-from flask import request
+import logging
 import google.auth
-from google.auth import iam
+
+from functools import wraps
+from flask import request
 from google.auth.transport.requests import Request as GRequest
-from google.oauth2.service_account import Credentials
-import jwt
-from requests import Request
+from google.oauth2 import id_token
 from requests import Session
+from requests import Request
 
-
-IAM_SCOPE = 'https://www.googleapis.com/auth/iam'
-OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
 HOST_HEADER = 'Forward-Host'
 
-_oidc_token = None
 _session = Session()
-_adc_credentials, _ = google.auth.default(scopes=[IAM_SCOPE])
 
 _whitelist = os.getenv('WHITELIST', [])
 if _whitelist:
     _whitelist = [p.strip() for p in _whitelist.split(',')]
 
+_client_id = os.getenv('CLIENT_ID')
 _username = os.getenv('AUTH_USERNAME')
 _password = os.getenv('AUTH_PASSWORD')
-
-# For service accounts using the Compute Engine metadata service, which is the
-# case for Cloud Function service accounts, service_account_email isn't
-# available until refresh is called.
-_adc_credentials.refresh(GRequest())
-
-# Since the Compute Engine metadata service doesn't expose the service
-# account key, we use the IAM signBlob API to sign instead. In order for this
-# to work, the Cloud Function's service account needs the "Service Account
-# Actor" role.
-_signer = iam.Signer(
-    GRequest(), _adc_credentials, _adc_credentials.service_account_email)
 
 
 def requires_auth(f):
@@ -53,19 +34,6 @@ def requires_auth(f):
                         {'WWW-Authenticate': 'Basic realm="Login Required"'})
         return f(*args, **kwargs)
     return decorated
-
-
-class OIDCToken(object):
-
-    def __init__(self, token_str):
-        self._token_str = token_str
-        self._claims = jwt.decode(token_str, verify=False)
-
-    def __str__(self):
-        return self._token_str
-
-    def is_expired(self):
-        return int(time.time()) >= self._claims['exp']
 
 
 @requires_auth
@@ -94,11 +62,7 @@ def handle_request(proxied_request):
             proxied_request.method, url))
         return 'Requested path {} not in whitelist'.format(path), 403
 
-    global _oidc_token
-    if not _oidc_token or _oidc_token.is_expired():
-        _oidc_token = _get_google_oidc_token()
-        logging.info('Renewed OIDC bearer token for {}'.format(
-            _adc_credentials.service_account_email))
+    _oidc_token = _get_google_oidc_token(_client_id)
 
     # Add the Authorization header with the OIDC token.
     headers['Authorization'] = 'Bearer {}'.format(_oidc_token)
@@ -121,42 +85,12 @@ def handle_request(proxied_request):
     return resp.content, resp.status_code, headers.items()
 
 
-def _get_google_oidc_token():
-    """Get an OpenID Connect token issued by Google for the environment's
-    service account.
-
-    This function:
-      1. Generates a JWT signed with the service account's private key
-         containing a special "target_audience" claim.
-
-      2. Sends it to the OAUTH_TOKEN_URI endpoint. Because the JWT in #1
-         has a target_audience claim, that endpoint will respond with
-         an OpenID Connect token for the service account -- in other words,
-         a JWT signed by *Google*. The aud claim in this JWT will be
-         set to the value from the target_audience claim in #1.
-
-    For more information, see
-    https://developers.google.com/identity/protocols/OAuth2ServiceAccount .
-    The HTTP/REST example on that page describes the JWT structure and
-    demonstrates how to call the token endpoint. (The example on that page
-    shows how to get an OAuth2 access token; this code is using a
-    modified version of it to get an OpenID Connect token.)
+def _get_google_oidc_token(client_id):
     """
-
-    credentials = Credentials(
-        _signer, _adc_credentials.service_account_email,
-        token_uri=OAUTH_TOKEN_URI,
-        additional_claims={'target_audience': os.getenv('CLIENT_ID')}
-    )
-    service_account_jwt = credentials._make_authorization_grant_assertion()
-    request = GRequest()
-    body = {
-        'assertion': service_account_jwt,
-        'grant_type': google.oauth2._client._JWT_GRANT_TYPE,
-    }
-    token_response = google.oauth2._client._token_endpoint_request(
-        request, OAUTH_TOKEN_URI, body)
-    return OIDCToken(token_response['id_token'])
+    Obtain an OpenID Connect (OIDC) token from metadata server or using service
+    account.
+    """
+    return id_token.fetch_id_token(GRequest(), client_id)
 
 
 _hoppish = {
